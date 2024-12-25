4ae2608709583cb560924f1c498eace28b34a693
依昔画家 <43005139+fevedy@users.noreply.github.com>
2024-12-25 16:30:49 +0800

    Update douban.py

--- douban2notion/douban.py
+++ douban2notion/douban.py
@@ -1,41 +1,23 @@
 import argparse
-import json
 import os
 import pendulum
 from retrying import retry
 import requests
 from douban2notion.notion_helper import NotionHelper
 from douban2notion import utils
-from douban2notion.config import movie_properties_type_dict, TAG_ICON_URL, USER_ICON_URL
+from douban2notion.config import movie_properties_type_dict, movie_status, rating
 from douban2notion.utils import get_icon
 from dotenv import load_dotenv
 
 load_dotenv()
 
-rating = {
-    1: "⭐️",
-    2: "⭐️⭐️",
-    3: "⭐️⭐️⭐️",
-    4: "⭐️⭐️⭐️⭐️",
-    5: "⭐️⭐️⭐️⭐️⭐️",
-}
-
-movie_status = {
-    "mark": "想看",
-    "doing": "在看",
-    "done": "看过",
-}
-
 AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 DOUBAN_API_HOST = os.getenv("DOUBAN_API_HOST", "frodo.douban.com")
 DOUBAN_API_KEY = os.getenv("DOUBAN_API_KEY", "0ac44ae016490db2204ce0a042db2916")
 
-if not AUTH_TOKEN:
-    raise Exception("AUTH_TOKEN is not set. Please set the environment variable correctly.")
-
 headers = {
     "host": DOUBAN_API_HOST,
-    "authorization": f"Bearer {AUTH_TOKEN}",
+    "authorization": f"Bearer {AUTH_TOKEN}" if AUTH_TOKEN else "",
     "user-agent": "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.16(0x18001023) NetType/WIFI Language/zh_CN",
     "referer": "https://servicewechat.com/wx2f9b06c1de1ccfca/84/page-frame.html",
 }
