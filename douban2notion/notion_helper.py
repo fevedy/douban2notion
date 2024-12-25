fb684f907bed9e9a9fbf4cbdea327c7bc0574fd7
依昔画家 <43005139+fevedy@users.noreply.github.com>
2024-12-25 16:03:23 +0800

    Update notion_helper.py

--- douban2notion/notion_helper.py
+++ douban2notion/notion_helper.py
@@ -25,19 +25,14 @@ BOOKMARK_ICON_URL = "https://www.notion.so/icons/bookmark_gray.svg"
 class NotionHelper:
     database_name_dict = {
         "MOVIE_DATABASE_NAME": "电影",
-        "BOOK_DATABASE_NAME": "书架",
-        "DAY_DATABASE_NAME": "日",
-        "WEEK_DATABASE_NAME": "周",
-        "MONTH_DATABASE_NAME": "月",
         "YEAR_DATABASE_NAME": "年",
-        "CATEGORY_DATABASE_NAME": "分类",
-        "DIRECTOR_DATABASE_NAME": "导演",
-        "AUTHOR_DATABASE_NAME": "作者",
+        "MONTH_DATABASE_NAME": "月",
     }
     database_id_dict = {}
     image_dict = {}
-    def __init__(self,type):
-        is_movie = True if type=="movie" else False
+
+    def __init__(self, type):
+        is_movie = True if type == "movie" else False
         page_url = os.getenv("NOTION_MOVIE_URL") if is_movie else os.getenv("NOTION_BOOK_URL")
         notion_token = os.getenv("NOTION_TOKEN")
         if not notion_token:
@@ -52,41 +47,22 @@ class NotionHelper:
         for key in self.database_name_dict.keys():
             if os.getenv(key) != None and os.getenv(key) != "":
                 self.database_name_dict[key] = os.getenv(key)
-        self.book_database_id = self.database_id_dict.get(
-            self.database_name_dict.get("BOOK_DATABASE_NAME")
-        )
         self.movie_database_id = self.database_id_dict.get(
             self.database_name_dict.get("MOVIE_DATABASE_NAME")
-        )
-        self.day_database_id = self.database_id_dict.get(
-            self.database_name_dict.get("DAY_DATABASE_NAME")
-        )
-        self.week_database_id = self.database_id_dict.get(
-            self.database_name_dict.get("WEEK_DATABASE_NAME")
-        )
-        self.month_database_id = self.database_id_dict.get(
-            self.database_name_dict.get("MONTH_DATABASE_NAME")
         )
         self.year_database_id = self.database_id_dict.get(
             self.database_name_dict.get("YEAR_DATABASE_NAME")
         )
-        self.category_database_id = self.database_id_dict.get(
-            self.database_name_dict.get("CATEGORY_DATABASE_NAME")
+        self.month_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("MONTH_DATABASE_NAME")
         )
-        self.director_database_id = self.database_id_dict.get(
-            self.database_name_dict.get("DIRECTOR_DATABASE_NAME")
-        )
-        self.author_database_id = self.database_id_dict.get(
-            self.database_name_dict.get("AUTHOR_DATABASE_NAME")
-        )
-        if self.day_database_id:
-            self.write_database_id(self.day_database_id)
 
     def write_database_id(self, database_id):
         env_file = os.getenv('GITHUB_ENV')
         # 将值写入环境文件
         with open(env_file, "a") as file:
             file.write(f"DATABASE_ID={database_id}\n")
+
     def extract_page_id(self, notion_url):
         # 正则表达式匹配 32 个字符的 Notion page_id
         match = re.search(
@@ -95,8 +71,6 @@ class NotionHelper:
         )
         if match:
             return match.group(0)
-        else:
-            raise Exception(f"获取NotionID失败，请检查输入的Url是否正确")
 
     @retry(stop_max_attempt_number=3, wait_fixed=5000)
     def search_database(self, block_id):
@@ -104,7 +78,6 @@ class NotionHelper:
         # 遍历子块
         for child in children:
             # 检查子块的类型
-
             if child["type"] == "child_database":
                 self.database_id_dict[
                     child.get("child_database").get("title")
@@ -131,14 +104,6 @@ class NotionHelper:
             week, self.week_database_id, TARGET_ICON_URL, properties
         )
 
-    def get_month_relation_id(self, date):
-        month = date.strftime("%Y年%-m月")
-        start, end = get_first_and_last_day_of_month(date)
-        properties = {"日期": get_date(format_date(start), format_date(end))}
-        return self.get_relation_id(
-            month, self.month_database_id, TARGET_ICON_URL, properties
-        )
-
     def get_year_relation_id(self, date):
         year = date.strftime("%Y")
         start, end = get_first_and_last_day_of_year(date)
@@ -147,31 +112,16 @@ class NotionHelper:
             year, self.year_database_id, TARGET_ICON_URL, properties
         )
 
-    def get_day_relation_id(self, date):
-        new_date = date.replace(hour=0, minute=0, second=0, microsecond=0)
-        day = new_date.strftime("%Y年%m月%d日")
-        properties = {
-            "日期": get_date(format_date(date)),
-        }
-        properties["年"] = get_relation(
-            [
-                self.get_year_relation_id(new_date),
-            ]
-        )
-        properties["月"] = get_relation(
-            [
-                self.get_month_relation_id(new_date),
-            ]
-        )
-        properties["周"] = get_relation(
-            [
-                self.get_week_relation_id(new_date),
-            ]
-        )
+    def get_month_relation_id(self, date):
+        year = date.strftime("%Y")
+        month = date.strftime("%m")
+        month_name = f"{year}年{month}月"
+        start, end = get_first_and_last_day_of_month(date)
+        properties = {"日期": get_date(format_date(start), format_date(end))}
         return self.get_relation_id(
-            day, self.day_database_id, TARGET_ICON_URL, properties
+            month_name, self.month_database_id, TARGET_ICON_URL, properties
         )
-    
+
     @retry(stop_max_attempt_number=3, wait_fixed=5000)
     def get_relation_id(self, name, id, icon, properties={}):
         key = f"{id}{name}"
@@ -189,12 +139,6 @@ class NotionHelper:
             page_id = response.get("results")[0].get("id")
         self.__cache[key] = page_id
         return page_id
-
-
-
-    @retry(stop_max_attempt_number=3, wait_fixed=5000)
-    def update_book_page(self, page_id, properties):
-        return self.client.pages.update(page_id=page_id, properties=properties)
 
     @retry(stop_max_attempt_number=3, wait_fixed=5000)
     def update_page(self, page_id, properties):
@@ -230,7 +174,6 @@ class NotionHelper:
     def delete_block(self, block_id):
         return self.client.blocks.delete(block_id=block_id)
 
-
     @retry(stop_max_attempt_number=3, wait_fixed=5000)
     def query_all_by_book(self, database_id, filter):
         results = []
@@ -243,46 +186,7 @@ class NotionHelper:
                 start_cursor=start_cursor,
                 page_size=100,
             )
-            start_cursor = response.get("next_cursor")
-            has_more = response.get("has_more")
             results.extend(response.get("results"))
-        return results
-
-    @retry(stop_max_attempt_number=3, wait_fixed=5000)
-    def query_all(self, database_id):
-        """获取database中所有的数据"""
-        results = []
-        has_more = True
-        start_cursor = None
-        while has_more:
-            response = self.client.databases.query(
-                database_id=database_id,
-                start_cursor=start_cursor,
-                page_size=100,
-            )
-            start_cursor = response.get("next_cursor")
             has_more = response.get("has_more")
-            results.extend(response.get("results"))
+            start_cursor = response.get("next_cursor")
         return results
-
-    def get_date_relation(self, properties, date):
-        properties["年"] = get_relation(
-            [
-                self.get_year_relation_id(date),
-            ]
-        )
-        properties["月"] = get_relation(
-            [
-                self.get_month_relation_id(date),
-            ]
-        )
-        properties["周"] = get_relation(
-            [
-                self.get_week_relation_id(date),
-            ]
-        )
-        properties["日"] = get_relation(
-            [
-                self.get_day_relation_id(date),
-            ]
-        )
